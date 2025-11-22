57a4317a9cc1039614c0585edfbf764cf6341f7d
jhoropez <jhoropeza_araujo@hotmail.com>
2025-11-22 14:18:28 +0100

    CAMBIAMOS EL NOMBRE DEL REPO

--- script_to_deploy_notebooks/deploy_notebooks.sql
+++ script_to_deploy_notebooks/deploy_notebooks.sql
@@ -13,14 +13,14 @@ Last Updated: 6/11/2024
 --USE SCHEMA {{env}}_SCHEMA;
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB_V2"."{{env}}_SCHEMA"."{{env}}_06_load_excel_files"')
-    FROM '@"DEMO_DB_V2"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/06_load_excel_files/'
+    FROM '@"DEMO_DB_V2"."INTEGRATIONS"."DEMO_GIT_REPO_V2"/branches/"{{branch}}"/notebooks/06_load_excel_files/'
     QUERY_WAREHOUSE = 'DEMO_WH'
     MAIN_FILE = '06_load_excel_files.ipynb';
 
 ALTER NOTEBOOK "DEMO_DB_V2"."{{env}}_SCHEMA"."{{env}}_06_load_excel_files" ADD LIVE VERSION FROM LAST;
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB_V2"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics"')
-    FROM '@"DEMO_DB_V2"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
+    FROM '@"DEMO_DB_V2"."INTEGRATIONS"."DEMO_GIT_REPO_V2"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
     QUERY_WAREHOUSE = 'DEMO_WH'
     MAIN_FILE = '07_load_daily_city_metrics.ipynb';
 
